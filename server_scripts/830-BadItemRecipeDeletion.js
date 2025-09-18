5a0a944140a513bae1a064270887180dc4262fcc
VirtualSofia <evasnowcraft@gmail.com>
2025-09-18 13:27:40 -0700

    removed some recipes

--- server_scripts/830-BadItemRecipeDeletion.js
+++ server_scripts/830-BadItemRecipeDeletion.js
@@ -1,3 +1,11 @@
 ServerEvents.recipes(event => {
-event.remove({ id: 'advancedperipherals:chunk_controller' })
+event.remove({ id: 'advancedperipherals:chunk_controller'}),
+event.remove({ id: 'create_mechanical_extruder:extruding/andesite'}),
+event.remove({ id: 'create_mechanical_extruder:extruding/granite'}),
+event.remove({ id: 'create_mechanical_extruder:extruding/diorite'}),
+event.remove({ id: 'create_mechanical_extruder:extruding/advanced_netherrack'}),
+event.remove({ id: 'create_mechanical_extruder:extruding/advanced_end_stone'}),
+event.remove({ id: 'create_mechanical_extruder:extruding/obsidian'}),
+event.remove({ id: 'create_mechanical_extruder:extruding/end_stone'})
+
 })
