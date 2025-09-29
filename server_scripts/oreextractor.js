73e7d7b380455c8322deea7cca489009cbfd9c99
VirtualSofia <evasnowcraft@gmail.com>
2025-09-29 00:28:13 -0700

    switched from extruders to drills

--- server_scripts/oreextractor.js
+++ server_scripts/oreextractor.js
@@ -8,7 +8,7 @@ event.custom(
   "type": "createoreexcavation:vein",
   "amountMultiplierMax": 2.0,
   "amountMultiplierMin": 1.0,
-  "biomeWhitelist": "minecraft:is_overworld",
+  "biomeWhitelist": "c:is_ocean",
   "finite": "always",
   "amountMultiplierMin": 0.5,
   "amountMultiplierMax": 0.20,
@@ -19,8 +19,8 @@ event.custom(
   "name": "{\"translate\":\"fluid.landfallessentials.crude_oil\"}",
   "placement": {
     "salt": 1195889335,
-    "separation": 8,
-    "spacing": 16
+    "separation": 16,
+    "spacing": 32
   },
   "priority": 0
 }
@@ -41,4 +41,40 @@ event.custom(
   "veinId": "landfall:oil_vein"
 }
 )
+	event.recipes.createoreexcavation.vein('{"text": "Gold Node"}', 'landfallessentials:extracted_gold_clump')
+		.placement(16, 8, 6894685).veinSize(3, 8.5).biomeWhitelist('landfall:renewable_gold')
+		.id("landfall:gold_vein")
+	event.recipes.createoreexcavation.drilling('landfallessentials:extracted_gold_clump', 'landfall:gold_vein', 100)
+		.stress(512).id("landfall:gold_extraction");
+
+	event.recipes.createoreexcavation.vein('{"text": "Copper Node"}', 'landfallessentials:extracted_copper_clump')
+        	.placement(16, 8, 6894685).veinSize(3, 8.5).biomeWhitelist('landfall:renewable_copper')
+        	.id("landfall:copper_vein")
+    event.recipes.createoreexcavation.drilling('landfallessentials:extracted_copper_clump', 'landfall:copper_vein', 100)
+        	.stress(512).id("landfall:copper_extraction");
+
+    event.recipes.createoreexcavation.vein('{"text": "Diamond Node"}', 'landfallessentials:extracted_diamond_clump')
+    		.placement(16, 8, 6894685).veinSize(3, 8.5).biomeWhitelist('landfall:renewable_diamond')
+    		.id("landfall:diamond_vein")
+    	event.recipes.createoreexcavation.drilling('landfallessentials:extracted_diamond_clump', 'landfall:diamond_vein', 100)
+    		.stress(512).id("landfall:diamond_extraction");
+
+
+    event.recipes.createoreexcavation.vein('{"text": "Lapis Node"}', 'landfallessentials:extracted_lapis_clump')
+    		.placement(16, 8, 6894685).veinSize(3, 8.5).biomeWhitelist('landfall:renewable_lapis')
+    		.id("landfall:lapis_vein")
+    	event.recipes.createoreexcavation.drilling('landfallessentials:extracted_lapis_clump', 'landfall:lapis_vein', 100)
+    		.stress(512).id("landfall:lapis_extraction");
+
+    event.recipes.createoreexcavation.vein('{"text": "Quartz Node"}', 'landfallessentials:extracted_quartz_clump')
+    		.placement(16, 8, 6894685).veinSize(3, 8.5).biomeWhitelist('landfall:renewable_quartz')
+    		.id("landfall:quartz_vein")
+    	event.recipes.createoreexcavation.drilling('landfallessentials:extracted_quartz_clump', 'landfall:quartz_vein', 100)
+    		.stress(512).id("landfall:quartz_extraction");
+
+    event.recipes.createoreexcavation.vein('{"text": "Zinc Node"}', 'landfallessentials:extracted_zinc_clump')
+    		.placement(4, 2, 6894685).veinSize(3, 8.5).biomeWhitelist('landfall:renewable_zinc')
+    		.id("landfall:zinc_vein")
+    	event.recipes.createoreexcavation.drilling('landfallessentials:extracted_zinc_clump', 'landfall:zinc_vein', 100)
+    		.stress(512).id("landfall:zinc_extraction");
 })
